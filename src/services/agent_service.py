741c414604e0aeeb2e3b2b47bdf212a782096582
13739186162 <120290590@qq.com>
2025-10-23 06:53:50 +0800

    send api joint debugging

--- src/services/agent_service.py
+++ src/services/agent_service.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 # 加载环境变量
