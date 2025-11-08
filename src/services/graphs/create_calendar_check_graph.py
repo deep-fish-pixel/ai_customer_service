e80ee433c5c03b2df82db76781fc7c093acad613
13739186162 <120290590@qq.com>
2025-11-08 10:45:41 +0800

    fix: 提供流程基本信息

--- src/services/graphs/create_calendar_check_graph.py
+++ src/services/graphs/create_calendar_check_graph.py
@@ -7,7 +7,7 @@ def create_calendar_check_graph() -> StateGraph:
 
     def collect_info(state):
         #提前5分钟、15分钟、1小时或1天提醒
-        return {"message": "请提供日程的标题、类型（会议、专注时间、私人事务）、参与者、会议类型（线上下会议：会议室名称，线上会议：会议链接）、日期、起止时间、 我将为您安排会议。"}
+        return {"message": "请提供日程的标题、类型（会议、专注时间、私人事务）、参与者、会议类型（线上下会议：会议室名称，线上会议：会议链接）、日期、起止时间(hh:mm)、 我将为您安排会议。"}
 
     graph.add_node("collect_info", collect_info)
     graph.set_entry_point("collect_info")
